e60743ca6501ccdcca7fffb1d1a659d28ec5cca1
jonel-hatwell <jonel.hatwell@gmail.com>
2025-12-16 18:16:52 +0800

    feat: orders api

---
+++ app/Http/Requests/Order/OrderUpdateRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'po_number' => 'required|string|max:50',
+            'client_id' => 'required|string|max:50',
+            'brand_id' => 'required|string|max:50',
+            'channel' => 'required|string|max:50',
+            'order_type' => 'required|string|max:50',
+            'design_name' => 'required|string|max:50',
+            'type_fabric' => 'required|string|max:50',
+            'type_size' => 'required|string|max:50',
+            'type_garment' => 'required|string|max:50',
+            'type_printing_method' => 'required|string|max:50',
+            'design_files' => 'string|max:50',
+            'artist_filename' => 'string|max:50',
+            'mockup_url' => 'string|max:50',
+            'mockup_images' => 'string|max:50',
+            'mockup_notes' => 'string|max:50',
+            'print_location' => 'string|max:50',
+            'total_quantity' => 'string|max:50',
+            'size_breakdown' => 'string|max:50',
+            'target_date' => 'string|max:50',
+            'instruction_files' => 'string|max:50',
+            'instruction_notes' => 'string|max:50',
+            'unit_price' => 'string|max:50',
+            'desposit_percentage' => 'string|max:50',
+            'payment_terms' => 'string|max:50',
+            'currency' => 'string|max:50',
+            'status' => 'string|max:50',
+        ];
+    }
+}
---
+++ app/Services/OrderService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Collection;
+
+
+class TypeGarmentService
+{
+    public function getAll(): Collection
+    {
+        return Order::all();
+    }
+
+    public function find(int $id): ?Order
+    {
+        return Order::find($id);
+    }
+
+	public function create(array $data): Order
+    {
+        return Order::create($data);
+    }
+
+    public function update(
+        Order $order,
+        array $data
+    ): Order {
+        $order->update($data);
+        return $order;
+    }
+
+    public function delete(Order $order): bool
+    {
+        return $order->delete();
+    }
+}
