14e88503f3d8c9632f662cec66d3da652c39a44f
h3rdy009 <160083808+h3rdy009@users.noreply.github.com>
2025-12-17 21:09:57 +0800

    po-items

--- app/Models/PoStatus.php
+++ app/Models/PoStatus.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PoStatus extends Model
 {
-    //
+    protected $table = 'po_status';
+
+    protected $fillable = [
+        'po_id',
+        'updated_by',
+        'message',
+    ];
+
 }
--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\Api\OrdersController;
 use App\Http\Controllers\Api\OrderProcessesController;
 use App\Http\Controllers\Api\OrdersPaymentController;
 use App\Http\Controllers\Api\PoStatusController;
+use App\Http\Controllers\Api\PoItemsController;
 
 
 // Route::get('/user', function (Request $request) {
@@ -47,6 +48,7 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('order-processes', OrderProcessesController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
     Route::apiResource('order-payments', OrdersPaymentController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
     Route::apiResource('po-statuses', PoStatusController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+    Route::apiResource('po-items', PoItemsController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
 
 });
 
