d127b829549fef906d003ac472346d1671069a8d
Bill Rula <fredricbill40@gmail.com>
2025-12-18 01:40:31 +0800

    feat(roles): Add roles for the 3 frontend

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,15 @@ use App\Http\Controllers\Api\FabricTypeController;
 use App\Http\Controllers\Api\TypeSizeController;
 use App\Http\Controllers\Api\WarehouseMaterialsController;
 use App\Http\Controllers\Api\ClientBrandController;
+use App\Http\MiddleWare\FrontendAccess;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class, 'register']);
+
 Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout']);
@@ -34,6 +36,26 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('type-size', TypeSizeController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
     Route::apiResource('client-brands', ClientBrandController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
     Route::apiResource('warehouse-materials', WarehouseMaterialsController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+});
+
+// example usage: localhost:8000/api/v2/user
+Route::prefix('v2')->group(function () {
+    Route::middleware(['auth:sanctum', 'frontend.access:ash', 'role:admin'])->group(function () {
+        Route::apiResource('user', UserController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
+        Route::apiResource('client', ClientController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
+        Route::apiResource('fabric-type', FabricTypeController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
+        Route::apiResource('type-size', TypeSizeController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
+        Route::apiResource('warehouse-materials', WarehouseMaterialsController::class)->only(['index', 'store', 'show', 'update', 'destroy']);
+        // Add more Ash-specific routes here
+    });
+
+    Route::middleware(['auth:sanctum', 'frontend.access:sorbetes', 'role:customer'])->group(function () {
+        Route::apiResource('client-brands', ClientBrandController::class) -> only(['index', 'store', 'show', 'update', 'destroy']);
+        // Add more Ash-specific routes here
+    });
+    Route::middleware(['auth:sanctum', 'frontend.access:reefer', 'role:customer'])->group(function () {
+        // Add more Ash-specific routes here
+    });
 });
 
 // Route::domain('admin.alphacentauri.com')->group(function () {
