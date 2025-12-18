18e77631d263b28dab2d586c8a57de417cc66678
Bill Rula <fredricbill40@gmail.com>
2025-12-18 17:11:02 +0800

    fix(po-status): Refactor and fix codes of po-status

--- app/Models/PoStatus.php
+++ app/Models/PoStatus.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Order;
+use App\Models\User;
 
 
 class PoStatus extends Model
 {
-    protected $table = 'po_status';
+    protected $table = 'po_statuses';
 
     protected $fillable = [
         'po_id',
@@ -19,5 +20,9 @@ class PoStatus extends Model
     public function order()
     {
          return $this->belongsTo(Order::class, 'po_id');
+    }    
+    public function user()
+    {
+         return $this->belongsTo(User::class, 'updated_by');
     }
 }
