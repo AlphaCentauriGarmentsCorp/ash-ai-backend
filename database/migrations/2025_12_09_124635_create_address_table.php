1594e70065efd706d444ee387484028e1b434973
Abdul De Borja <abduldbdev@gmail.com>
2026-01-29 04:27:13 +0800

    fix(DB): Fix DB Migration

--- database/migrations/2025_12_09_124635_create_address_table.php
+++ database/migrations/2025_12_09_124635_create_address_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_address', function (Blueprint $table) {
             $table->id();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('street')->nullable();
             $table->string('province')->nullable();
             $table->string('city')->nullable();
