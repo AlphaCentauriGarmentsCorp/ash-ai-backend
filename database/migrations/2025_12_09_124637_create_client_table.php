d66822e6efb8741476b48793ef3175fa270aaa7c
Abdul De Borja <abduldbdev@gmail.com>
2026-02-10 12:51:40 +0800

    feat(Clients): Client Seeder

--- database/migrations/2025_12_09_124637_create_client_table.php
+++ database/migrations/2025_12_09_124637_create_client_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('contact_number');
-            $table->string('address');
+            $table->string('email')->nullable();
+            $table->string('contact_number')->nullable();
+            $table->string('address')->nullable();
             $table->text('notes')->nullable();
             $table->timestamps();
         });
