a9778a18da9c93eb5532f589df1e4988ee3b675c
Keilizon-Deiv D. Leones <keilizonleones14@gmail.com>
2026-03-12 14:07:55 +0800

    fix(placement-measurement): align show with route

--- app/Http/Requests/PlacementMeasurement/Update.php
+++ app/Http/Requests/PlacementMeasurement/Update.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class PlacementMeasurementStoreRequest extends FormRequest
+class Update extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -41,17 +41,4 @@ class PlacementMeasurementStoreRequest extends FormRequest
         ];
     }
 
-    /**
-     * Handle a failed validation attempt.
-     */
-    protected function failedValidation(Validator $validator): void
-    {
-        throw new HttpResponseException(
-            response()->json([
-                'success' => false,
-                'message' => 'Validation errors',
-                'errors'  => $validator->errors(),
-            ], 422)
-        );
-    }
 }
