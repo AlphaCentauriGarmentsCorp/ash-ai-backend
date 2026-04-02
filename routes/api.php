3be81a5157cc4f6193f837fb075ef78b3799f70b
Ken <opiana.218936@novaliches.sti.edu.ph>
2026-04-02 18:40:09 +0800

    feat(CourierList): Add CRUD REST API for Courier List

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ use App\Http\Controllers\Api\OrderStagesController;
 use App\Http\Controllers\Api\ScreenCheckingController;
 use App\Http\Controllers\Api\ScreenMakingController;
 use App\Http\Controllers\Api\ScreenMaintenanceController;
+use App\Http\Controllers\Api\CourierListController;
 
 // example usage: localhost:8000/api/v1/user
 // Route::prefix('v1')->group(function () {
@@ -199,8 +200,15 @@ Route::prefix('v2')->group(function () {
             Route::delete('/{id}', 'destroy');
         });
 
-
         Route::prefix('/screens')->controller(ScreenController::class)->group(function () {
+            Route::get('/', 'index');
+            Route::post('/', 'store');
+            Route::get('/{id}', 'show');
+            Route::put('/{id}', 'update');
+            Route::delete('/{id}', 'destroy');
+        });
+
+        Route::prefix('/courier-list')->controller(CourierListController::class)->group(function () {
             Route::get('/', 'index');
             Route::post('/', 'store');
             Route::get('/{id}', 'show');
